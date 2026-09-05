b932027550858d429d10eaf2b2e404d90b9950f3
Kyle Mac <hello@mrkylemac.com>
2026-09-05 22:02:52 +1000

    Update home-client.tsx

--- src/app/home-client.tsx
+++ src/app/home-client.tsx
@@ -7,7 +7,7 @@ import { VENUES } from '@/config/api';
 import { getAllCachedEntries, getCacheKey } from '@/lib/venueCache';
 import { Card, CardContent } from '@/components/ui/card';
 
-const FEATURED_IDS = ['innerstudio', '59636', '49448', 'alchemysaunas', 'navia'];
+const FEATURED_IDS = ['innerstudio', '59636', '49448', 'alchemysaunas', 'navia', '41275'];
 
 export function HomeClient() {
   const router = useRouter();
